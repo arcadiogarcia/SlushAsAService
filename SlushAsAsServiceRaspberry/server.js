e09b1689c1335c97b8f614624eeb5551d945a0ae
Arcadio García <49.pyme@garciamontoro.com>
2015-05-21 19:15:37 +0200

    Raspberry client logic

--- SlushAsAsServiceRaspberry/server.js
+++ SlushAsAsServiceRaspberry/server.js
@@ -3,23 +3,35 @@ var gpio = require("pi-gpio");
 var io = require('socket.io-client');
 var socket = io.connect("http://slushasaservice.azurewebsites.net",{reconnect:true});
 
+var intervalHolder;
+
+function output(value, callback){
+    gpio.open(7,"output",function(err){
+            gpio.write(7,value,function(){
+                gpio.close(7);
+                if(callback){
+                    callback();
+                }
+            });
+    });
+}
+
 socket.on("connect", function(socket){
    socket.on("event",function(data){
-       
+       switch(data.action){
+			case "start":
+				output(1,function(){
+                          intervalHolder = setTimeout(function(){
+                                output(0);
+                                socket.broadcast.emit('event', {"action":"status","status":"stop"}); 
+                          },data.time);
+                 });
+                 socket.broadcast.emit('event', {"action":"status","status":"start","time":data.time}); 
+			break;
+			case "stop":
+				output(0);
+				socket.broadcast.emit('event', {"action":"status","status":"stop"}); 
+			break;
+       }
    });
-});
-
-console.log("started");
-gpio.open(7,"output",function(err){
-	console.log("opened");
-    gpio.write(7,1,function(){
-		console.log("1");
-       setTimeout(function(){
-       gpio.write(7,0, function(){
-		console.log("0");
-       gpio.close(7);
-	   console.log("closed");
-       });
-	},1000);
-       });
 });
